e7f3d08fc992c16e0bb9f6da7391f5e4462be336
DHIYANESH21 <dhiyaneshsakthivel@gmail.com>
2025-02-01 10:38:45 +0530

    UI

--- frontend/componets/Navbar.jsx
+++ frontend/componets/Navbar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-orange-500 flex justify-between'>
+        <h1 className='font-sans text-white ml-5 p-3 text-lg font-bold text-2xl'>E-sandhai</h1>
+        <div className='mr-5 p-3'><button>signin</button></div>
+    </div>
   )
 }
 
